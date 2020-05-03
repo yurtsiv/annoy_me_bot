ee6c70387c4b0f197fd313376470bfca2a87fc24
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-05-03 19:58:48 +0200

    add support for ringing annoyance

--- bot.js
+++ bot.js
@@ -1,8 +1,14 @@
+const mqtt = require('mqtt')
 const { Telegraf } = require('telegraf')
  
+const mqtt_client  = mqtt.connect('mqtt://test.mosquitto.org')
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.start((ctx) => ctx.reply("Welcome! Please don't annoy me too much"))
+
+bot.command('door_bell', (ctx) => {
+  mqtt_client.publish('home/ding_dong');
+  ctx.reply("Ringing into Stepan's door bell")
+});
+
 bot.launch()
